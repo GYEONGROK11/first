e4a64bbc266fb10b266b1acc3159e31c33eeb924
gpol2300 <gpol123@naver.com>
2024-03-26 18:13:52 +0900

    Q 오일러 문제 풀어보기

---
+++ src/main/java/extra/Q/Q1.java
@@ -0,0 +1,40 @@
+package extra.Q;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        int num = 0;
+        System.out.print("최댓값을 입력하세요 (최대 9999) >>");
+        Scanner scan = new Scanner(System.in);
+        num = scan.nextInt();
+        int a, b, c, d;
+        int sum;
+        for (int i = 1; i <= num; i++) {
+            d = i % 10;
+            i /= 10;
+            c = i % 10;
+            i /= 10;
+            b = i % 10;
+            i /= 10;
+            a = i % 10;
+            i /= 10;
+            int d1 = d * d;
+            int c1 = c * c;
+            int b1 = b * b;
+            int a1 = a * a;
+
+            sum +=d1;
+            sum +=c1;
+            sum +=b1;
+            sum +=a1;
+
+            while (sum )
+
+            }
+
+
+
+        }
+    }
+}
